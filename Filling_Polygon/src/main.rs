add3cb2da5592146b2439608c6d2fe563b53d4ff
Bianca <cal22272@uvg.edu.gt>
2024-07-17 18:36:01 -0600

    feat: polygon 2, blue with white putline added

--- Filling_Polygon/src/main.rs
+++ Filling_Polygon/src/main.rs
@@ -18,7 +18,7 @@ fn main() {
     framebuffer.clear();
 
     // Define the vertices of the polygon 1
-    let vertices = vec![
+    let vertices_1 = vec![
         Vec3::new(165.0, 380.0, 0.0),
         Vec3::new(185.0, 360.0, 0.0),
         Vec3::new(180.0, 330.0, 0.0),
@@ -31,13 +31,29 @@ fn main() {
         Vec3::new(193.0, 383.0, 0.0),
     ];
 
-    // Filling the polygon 1 with yellow color
-    framebuffer.fill_polygon(&vertices, Color::new(255, 255, 0));
+     // Define the vertices of the polygon 2
+     let vertices_2 = vec![
+        Vec3::new(321.0, 335.0, 0.0),
+        Vec3::new(288.0, 286.0, 0.0),
+        Vec3::new(339.0, 251.0, 0.0),
+        Vec3::new(374.0, 302.0, 0.0),
+    ];
 
-    // Setting the current drawing color to white and draw the polygon's edges
+    // Fill the polygon 1 with yellow color
+    framebuffer.fill_polygon(&vertices_1, Color::new(255, 255, 0));
+
+    // Set the current drawing color to white and draw the polygon's edges
     framebuffer.set_current_color(0xFFFFFF);
-    framebuffer.draw_polygon(&vertices);
+    framebuffer.draw_polygon(&vertices_1);
+
+
+    // Fill the polygon 2 with blue color
+    framebuffer.fill_polygon(&vertices_2, Color::new(0, 0, 255));
+
+    // Set the current drawing color to white and draw the polygon's edges
+    framebuffer.set_current_color(0xFFFFFF);
+    framebuffer.draw_polygon(&vertices_2);
 
     // Save the framebuffer as a BMP file
-    framebuffer.render_buffer("poligon1.bmp").expect("Failed to render buffer");
+    framebuffer.render_buffer("out.bmp").expect("Failed to render buffer");
 }
