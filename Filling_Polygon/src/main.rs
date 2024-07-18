d06c0fc9a9e82470b7a0d44682ae2507cfb75871
Bianca <cal22272@uvg.edu.gt>
2024-07-17 18:41:17 -0600

    poligon 3 added, red with white outline

--- Filling_Polygon/src/main.rs
+++ Filling_Polygon/src/main.rs
@@ -17,6 +17,9 @@ fn main() {
     framebuffer.set_background_color(0x0000000);
     framebuffer.clear();
 
+    // Set the current drawing color to white for the borders
+    framebuffer.set_current_color(0xFFFFFF);
+
     // Define the vertices of the polygon 1
     let vertices_1 = vec![
         Vec3::new(165.0, 380.0, 0.0),
@@ -31,28 +34,35 @@ fn main() {
         Vec3::new(193.0, 383.0, 0.0),
     ];
 
-     // Define the vertices of the polygon 2
-     let vertices_2 = vec![
+    // Fill the polygon 1 with yellow color
+    framebuffer.fill_polygon(&vertices_1, Color::new(255, 255, 0));
+    // Draw the polygon 1's edges with white color
+    framebuffer.draw_polygon(&vertices_1);
+
+    // Define the vertices of the polygon 2
+    let vertices_2 = vec![
         Vec3::new(321.0, 335.0, 0.0),
         Vec3::new(288.0, 286.0, 0.0),
         Vec3::new(339.0, 251.0, 0.0),
         Vec3::new(374.0, 302.0, 0.0),
     ];
 
-    // Fill the polygon 1 with yellow color
-    framebuffer.fill_polygon(&vertices_1, Color::new(255, 255, 0));
-
-    // Set the current drawing color to white and draw the polygon's edges
-    framebuffer.set_current_color(0xFFFFFF);
-    framebuffer.draw_polygon(&vertices_1);
-
-
     // Fill the polygon 2 with blue color
     framebuffer.fill_polygon(&vertices_2, Color::new(0, 0, 255));
-
-    // Set the current drawing color to white and draw the polygon's edges
-    framebuffer.set_current_color(0xFFFFFF);
+    // Draw the polygon 2's edges with white color
     framebuffer.draw_polygon(&vertices_2);
+
+    // Define the vertices of the polygon 3
+    let vertices_3 = vec![
+        Vec3::new(377.0, 249.0, 0.0),
+        Vec3::new(411.0, 197.0, 0.0),
+        Vec3::new(436.0, 249.0, 0.0),
+    ];
+
+    // Fill the polygon 3 with red color
+    framebuffer.fill_polygon(&vertices_3, Color::new(255, 0, 0));
+    // Draw the polygon 3's edges with white color
+    framebuffer.draw_polygon(&vertices_3);
 
     // Save the framebuffer as a BMP file
     framebuffer.render_buffer("out.bmp").expect("Failed to render buffer");
