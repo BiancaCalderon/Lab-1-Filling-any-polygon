4ba180f044c5640a2ad66acbd825ad1b09b2d52d
Bianca <cal22272@uvg.edu.gt>
2024-11-25 13:48:54 -0600

    feat: cleaning code

--- Filling_Polygon/target/debug/deps/smallvec-63d36f02fdf4728b.d
+++
@@ -1,7 +0,0 @@
-/home/bianca_cal/Lab-1-Filling-any-polygon/Filling_Polygon/target/debug/deps/libsmallvec-63d36f02fdf4728b.rmeta: /home/bianca_cal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/smallvec-1.13.2/src/lib.rs
-
-/home/bianca_cal/Lab-1-Filling-any-polygon/Filling_Polygon/target/debug/deps/libsmallvec-63d36f02fdf4728b.rlib: /home/bianca_cal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/smallvec-1.13.2/src/lib.rs
-
-/home/bianca_cal/Lab-1-Filling-any-polygon/Filling_Polygon/target/debug/deps/smallvec-63d36f02fdf4728b.d: /home/bianca_cal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/smallvec-1.13.2/src/lib.rs
-
-/home/bianca_cal/.cargo/registry/src/index.crates.io-6f17d22bba15001f/smallvec-1.13.2/src/lib.rs:
